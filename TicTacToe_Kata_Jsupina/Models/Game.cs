92af51bc774a016ee31964457bbf4f4ef3949c52
John Supina <jsupina32@aim.com>
2019-03-29 18:37:51 -0400

    Changes for refactoring models

--- TicTacToe_Kata_Jsupina/Models/Game.cs
+++ TicTacToe_Kata_Jsupina/Models/Game.cs
@@ -10,8 +10,91 @@ namespace TicTacToe_Kata_Jsupina.Models
         public List<Player> Participants { get; set; }
         public List<Tile> AvailableTiles { get; set; }
         public Player Winner { get; set; }
-        public int TurnNum{ get; set; }
+        public int TurnNum { get; set; }
         public bool InProgress { get; set; }
         public string LastMove { get; set; }
+
+        public Game()
+        {
+            List<string> tileNames = new List<string> { "Top Left", "Top Center", "Top Right", "Middle Left", "Middle Center",
+                                                        "Middle Right", "Bottom Left", "Bottom Center", "Bottom Right"};
+            List<string> playerMarks = new List<string> { "X", "O" };
+            AvailableTiles = new List<Tile>();
+            Participants = new List<Player>();
+            Winner = null;
+            TurnNum = 0;
+            InProgress = true;
+            LastMove = "";
+
+            for (int x = 0; x < tileNames.Count; x++)
+            {
+                var tile = new Tile
+                {
+                    TileID = x,
+                    TileName = tileNames[x]
+                };
+
+                AvailableTiles.Add(tile);
+            }
+
+            for (int y = 0; y < playerMarks.Count; y++)
+            {
+                var player = new Player
+                {
+                    PlayerID = y,
+                    PlayerMark = playerMarks[y],
+                    HeldTiles = new List<Tile>()
+                    
+                };
+
+                Participants.Add(player);
+            }
+        }
+
+        public bool ValidMove(int tileNum)
+        {
+            //Check if the intended move is still available
+            if (AvailableTiles.Any(p => p.TileID == tileNum) == false)
+            {
+                //The requested move is invalid, thus re-direct back to the view for
+                //the player to make another move
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        public void ProcessMove(int moveNum)
+        {
+            int whosTurn = TurnNum % 2;
+            //Pull the tile that the player selected
+            var tileToRemove = AvailableTiles.Single(t => t.TileID == moveNum);
+            //Pull the current player
+            var currentPlayer = Participants.Find(p => p.PlayerID == whosTurn);
+
+            //"Give" the player possesion of the tile and remove it from available move options
+            currentPlayer.HeldTiles.Add(tileToRemove);
+            AvailableTiles.Remove(tileToRemove);
+
+            //Update the most recent move so that a moe history could be recorded and increase the turn counter 
+            LastMove = "Player " + currentPlayer.PlayerMark + " put their mark on the " + tileToRemove.TileName + " tile!";
+            TurnNum = TurnNum + 1;
+
+            if(currentPlayer.CheckForWin() == true)
+            {
+                InProgress = false;
+                Winner = currentPlayer;
+            }
+
+            //Check if there are no more available tiles. If there are no more tiles after not satisfying a win
+            //condition, end the game and leave the winner null.
+            else if (AvailableTiles.Count == 0)
+            {
+                InProgress = false;
+            }
+        }
     }
 }
--- TicTacToe_Kata_Jsupina/Models/Player.cs
+++ TicTacToe_Kata_Jsupina/Models/Player.cs
@@ -10,5 +10,61 @@ namespace TicTacToe_Kata_Jsupina.Models
         public int PlayerID { get; set; }
         public string PlayerMark { get; set; }
         public List<Tile> HeldTiles { get; set; }
+
+        public bool CheckForWin()
+        {
+            if (HeldTiles.Any(t => t.TileID == 0) == true && HeldTiles.Any(t => t.TileID == 1) == true &&
+                HeldTiles.Any(t => t.TileID == 2) == true)
+            {
+                return true;
+            }
+
+            else if (HeldTiles.Any(t => t.TileID == 3) == true && HeldTiles.Any(t => t.TileID == 4) == true &&
+                     HeldTiles.Any(t => t.TileID == 5) == true)
+            {
+                return true;
+            }
+
+            else if (HeldTiles.Any(t => t.TileID == 6) == true && HeldTiles.Any(t => t.TileID == 7) == true &&
+                     HeldTiles.Any(t => t.TileID == 8) == true)
+            {
+                return true;
+            }
+
+            else if (HeldTiles.Any(t => t.TileID == 0) == true && HeldTiles.Any(t => t.TileID == 3) == true &&
+                     HeldTiles.Any(t => t.TileID == 6) == true)
+            {
+                return true;
+            }
+
+            else if (HeldTiles.Any(t => t.TileID == 1) == true && HeldTiles.Any(t => t.TileID == 4) == true &&
+                     HeldTiles.Any(t => t.TileID == 7) == true)
+            {
+                return true;
+            }
+
+            else if (HeldTiles.Any(t => t.TileID == 2) == true && HeldTiles.Any(t => t.TileID == 5) == true &&
+                     HeldTiles.Any(t => t.TileID == 8) == true)
+            {
+                return true;
+            }
+
+            else if (HeldTiles.Any(t => t.TileID == 0) == true && HeldTiles.Any(t => t.TileID == 4) == true &&
+                     HeldTiles.Any(t => t.TileID == 8) == true)
+            {
+                return true;
+            }
+
+            else if (HeldTiles.Any(t => t.TileID == 2) == true && HeldTiles.Any(t => t.TileID == 4) == true &&
+                     HeldTiles.Any(t => t.TileID == 6) == true)
+            {
+                return true;
+            }
+            
+            else
+            {
+                return false;
+            }
+        }
     }
 }
